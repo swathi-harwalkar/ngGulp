8ee700c57fda1d0ad97a4a68baa367d618386f79
sunil-mane <sunil_mane@yahoo.com>
2016-11-08 11:41:46 +0530

    ui data bindings

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,16 +2,22 @@
   'use strict';
 
   angular
-    .module('psi')
-    .controller('MainController', MainController);
+  .module('psi')
+  .controller('MainController', MainController);
 
   /** @ngInject */
   function MainController(TestCenterService) {
     var vm = this;
 
-    TestCenterService.search().then(function(response){
-      vm.testCenters =  response;   
-    });
+    vm.searchObj = {};
+
+    vm.searchObj.distance  = "5";
+
+    vm.search = function(){
+      TestCenterService.search().then(function(response){
+        vm.testCenters =  response;   
+      });
+    }
     
   }
 })();
